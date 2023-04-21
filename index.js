c00462659eafe8daec84042706eeafde7e257b5c
Ajay Kumar B <b.ajaykumar306k@gmail.com>
2023-04-21 23:10:10 +0530

    updated form

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 const path = require('path')
-var corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+// var corsOptions = {
+//     origin: '*',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }
 
-  app.use(cors(corsOptions))
+  // app.use(cors(corsOptions))
 
 const dotenv = require('dotenv');
 dotenv.config();
